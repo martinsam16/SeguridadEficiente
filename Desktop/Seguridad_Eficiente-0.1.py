0aadcac91af1f4274574343959e56dce708b9a81
master <{martin.saman@vallegrande.edu.pe}>
2018-09-26 03:02:49 -0500

    explicando por comentarios

--- Desktop/Seguridad_Eficiente-0.1.py
+++ Desktop/Seguridad_Eficiente-0.1.py
@@ -1,21 +1,23 @@
-import cv2
-import numpy as np
+#Importando librerias
+import cv2#Visión Artificial
+import numpy as np #Procesamiento con matrices
 
+#Importando Cascades
 clasificador_ojo=cv2.CascadeClassifier('Haarcascade/haarcascade_eye.xml')
 clasificador_rostro=cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_alt.xml')
-capturar=cv2.VideoCapture(0)
+capturar=cv2.VideoCapture(0) #Iniciar captura de video, donde (n) es el número de cámara escogida
 
 while(True):
-        a, frame = capturar.read()
-        imagen=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        a, frame = capturar.read() #a es un booleano; frame, las imágenes
+        imagen=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#cambiar colores a escala de grises y guardar en imagen
         ojos = clasificador_ojo.detectMultiScale(imagen, 1.3, 5)
         rostro = clasificador_rostro.detectMultiScale(imagen, 1.3, 5)
         for (x,y,w,h) in ojos:
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),3)
         for (x,y,w,h) in rostro:
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),5)
-        cv2.imshow('Seguridad Eficiente', frame)
-        if cv2.waitKey(1) & 0xFF == ord('s'):
-                break
+        cv2.imshow('Seguridad Eficiente', frame) #Crear ventana con: (nombre de la ventana, objeto a mostrar)
+        if cv2.waitKey(1) & 0xFF == ord('s'):#Esperar que una tecla se presione y que esta sea la 's'
+                break#Saltar bucle
 capturar.release()
-cv2.destroyAllWindows()
+destroyWindow('Seguridad Eficiente')
