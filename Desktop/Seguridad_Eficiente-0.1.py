00a097e0831ca47fb468c10a952ae941c87a1bcf
master <{martin.saman@vallegrande.edu.pe}>
2018-09-26 02:29:11 -0500

    asaaa

--- Desktop/Seguridad_Eficiente-0.1.py
+++ Desktop/Seguridad_Eficiente-0.1.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 #import matplotlib.pyplot as plt#
 #ads
-asass
+#####asass
 clasificador_ojo=cv2.CascadeClassifier('Haarcascade/haarcascade_eye.xml')
 clasificador_rostro=cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_alt.xml')
 capturar=cv2.VideoCapture(0)
