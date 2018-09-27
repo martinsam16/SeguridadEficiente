5e1dd2f039f2d52564ab9bb27d9ee69d1c469ce1
master <{martin.saman@vallegrande.edu.pe}>
2018-09-26 19:42:47 -0500

    Cortando cuadros de rostros autodetectados

--- Desktop/Seguridad_Eficiente-0.1.py
+++ Desktop/Seguridad_Eficiente-0.1.py
@@ -5,35 +5,25 @@ import numpy as np #Procesamiento con matrices
 #Importando Archivos clasificadores
 clasificador_ojo=cv2.CascadeClassifier('Haarcascade/haarcascade_eye.xml')
 clasificador_rostro=cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_alt.xml')
-capturar=cv2.VideoCapture(0) #Iniciar captura de video, donde (n) es el número de cámara escogida
+capturar=cv2.VideoCapture(0) #Inicializar cámara (n) es el número de cámara escogida
 
 while(True):#Bucle infinito
-        a, frame = capturar.read() #a es un booleano; frame, las imágenes
+        a, frame = capturar.read()#leo con la camara seleccionada:a es booleano, frame es un cuadro
         imagen=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#cambiar colores a escala de grises y guardar en imagen
         ojos = clasificador_ojo.detectMultiScale(imagen, 1.3, 5)
         rostro = clasificador_rostro.detectMultiScale(imagen, 1.3, 5)
+
         for (x,y,w,h) in ojos:
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),3) #(x1,y1) - vertices del cuadro
         for (x,y,w,h) in rostro:
+                cortar= imagen[y:y+h, x:x+w]#Cortar coordenadas seleccionadas
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),5)
-                cv2.putText(frame, 'Martin Alexis', (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN,1,(300, 255, 0))
- 
-        #Intentando recortar rostro ----------------
+                cv2.putText(frame, 'Nombre', (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN,1,(300, 255, 0))
 
-        """cortar = imagen[70:170, 440:540]
-        cv2.imshow("Cortar", cortar)"""
-
-        """
-        tamano=4
-        redimensionar = cv2.resize(imagen, (int(imagen.shape[1] / tamano), int(imagen.shape[0] / tamano)))
-        recortado = clasificador_rostro.detectMultiScale(redimensionar)
-        recortado = sorted(recortado, key=lambda x: x[3])
-        """
-        #---------------------------------------------
         cv2.imshow('Seguridad Eficiente', frame) #Crear ventana con: (nombre de la ventana, objeto a mostrar)
 
         if cv2.waitKey(1) & 0xFF == ord('g'):#Esperar que una tecla se presione y que esta sea la 'g'
-                cv2.imwrite('img/captura.png',imagen)#Escribir o guardar imagen (poner nombre y la extensión, imagen)
+                cv2.imwrite('img/captura.png',cortar)#Escribir o guardar imagen (poner nombre y la extensión, imagen)
         elif cv2.waitKey(1) & 0xFF == ord('s'):#Esperar que una tecla se presione y que esta sea la 's'
                 break#Saltar bucle
 
