5026cc6770b5a7df4045b224173f533641b5b7ba
Josue Cuba <42879174+JosueCuba@users.noreply.github.com>
2018-09-28 09:50:27 -0500

    Update BD_Rostros.sql

--- BD_Rostros.sql
+++ BD_Rostros.sql
@@ -27,13 +27,13 @@ CREATE TABLE IF NOT EXISTS `PROYECTO`.`user` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `PROYECTO`.`Usuario` (
   `idUsuario` INT NOT NULL, /*codigo de usuario*/
-  `NomUser` VARCHAR(45) NULL, /*nombre del usuario que es gravado o tomado foto*/
-  `ApellUser` VARCHAR(45) NULL,/*Apellido del usuario que es gravado o tomado foto*/
-  `DNIUser` VARCHAR(8) NULL,/* DNI  del usuario que es gravado o tomado foto*/
+  `NomUser` VARCHAR(45) NOT NULL, /*nombre del usuario que es gravado o tomado foto*/
+  `ApellUser` VARCHAR(45) NOT  NULL,/*Apellido del usuario que es gravado o tomado foto*/
+  `DNIUser` VARCHAR(8) NOT NULL,/* DNI  del usuario que es gravado o tomado foto*/
   `FechNacUSer` VARCHAR(45) NULL,/* Fecha de nacimiento del usuario del usuario que es gravado o tomado foto*/
   `DirecUser` VARCHAR(45) NULL,/*monbre del usuario que es gravado o tomado foto*/
   `NroCellUser` VARCHAR(9) NULL,/* Celular  del usuario que es gravado o tomado foto*/
-  `GenrUser` VARCHAR(1) NULL,/* Genero  del usuario que es gravado o tomado foto*/
+  `GenrUser` VARCHAR(1) NOT NULL,/* Genero  del usuario que es gravado o tomado foto*/
   `FotoUser` VARCHAR(45) NULL,/* foto del usuario */
   `PuntoFacialUser` VARCHAR(45) NULL, /*los punto facial */
   constraint idusuario_PK PRIMARY KEY (`idUsuario`)) 
