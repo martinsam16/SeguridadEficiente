5f5c3414eabdf59c1a389575e614431ed1a3fbc0
master <{martin.saman@vallegrande.edu.pe}>
2018-09-29 23:47:41 -0500

    a

--- Desktop/Seguridad_Eficiente-0.1.py
+++ Desktop/Seguridad_Eficiente-0.1.py
@@ -21,26 +21,26 @@ while(True):#Bucle infinito
         registro = open('registro.txt','a')# a escribe desde el final, w bora y escribe
 
         for (x,y,w,h) in ojos:#Vertices del cuadro y coordenadas
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),3)
+                #cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),3)
                 print "Ojo   :"+str([x,y])+"#"+str([[x,w],y])+"#"+str([x,[y,w]])+"#"+str([[x,w]+ [y,h]])
                 #registro.write(str(time.strftime("OJO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S"))+'\n')
                 #print str(time.strftime("OJO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S"))+'\n'
         for (x,y,w,h) in rostro:
-                #--cortar= imagen[y:y+h, x:x+w]#Cortar coordenadas seleccionadas
-                #cv2.imshow('Rostros', cortar)
+                cortar= imagen[y:y+h, x:x+w]#Cortar coordenadas seleccionadas
+                cv2.imshow('Rostros', cortar)
                 #print x+w+y+h
                 
-                print "Rostro:"+str([x,y])+"#"+str([[x,w],y])+"#"+str([x,[y,w]])+"#"+str([[x,w]+ [y,h]])
-                print "//////"
-                #registro.write(cortar+'\n')
-                #print str(time.strftime("ROSTRO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S"))+'\n'                
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),5)
+                #print "Rostro:"+str([x,y])+"#"+str([[x,w],y])+"#"+str([x,[y,w]])+"#"+str([[x,w]+ [y,h]])
+                #print "//////"
+                
+                registro.write(time.strftime("ROSTRO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S")+'\n')                
+                #cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),5)
                 cv2.putText(frame, Nombre, (x-5, y-5), cv2.FONT_HERSHEY_PLAIN,1,(300, 255, 0))#Insertar texto
 
         cv2.imshow('Seguridad Eficiente V-0.0', frame) #Crear ventana con: (nombre de la ventana, objeto a mostrar)
         
         if cv2.waitKey(1) & 0xFF == ord('g'):#Esperar que una tecla se presione y que esta sea la 'g'
-                #--cv2.imwrite('img/'+Nombre+str(nfoto)+'.png',cortar)#Escribir o guardar imagen (poner nombre y la extensión, imagen)
+                cv2.imwrite('img/'+Nombre+str(nfoto)+'.png',cortar)#Escribir o guardar imagen (poner nombre y la extensión, imagen)
                 nfoto+=1
         if cv2.waitKey(1) & 0xFF == ord('s'):#Esperar que una tecla se presione y que esta sea la 's'
                 break#Saltar bucle
@@ -49,3 +49,4 @@ capturar.release()#Liberando recursos
 registro.close()
 #cv2.destroyWindow('Seguridad Eficiente')#Cerrar la ventana que tiene (este nombre)
 cv2.destroyAllWindows()#Cerrar todas las ventanas creadas
+exit()
