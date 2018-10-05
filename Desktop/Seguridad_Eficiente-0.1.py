ce8931ffff739dafcd73d35fe2c57177a376eb06
master <{martin.saman@vallegrande.edu.pe}>
2018-10-05 00:19:44 -0500

    cc

--- Desktop/Seguridad_Eficiente-0.1.py
+++ Desktop/Seguridad_Eficiente-0.1.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 #import serial
 
 #Importando Archivos clasificadores
-clasificador_ojo=cv2.CascadeClassifier('Haarcascade/haarcascade_eye.xml')
+#clasificador_ojo=cv2.CascadeClassifier('Haarcascade/haarcascade_eye.xml')
 clasificador_rostro=cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_alt.xml')
 capturar=cv2.VideoCapture(0) #Inicializar cámara (n) es el número de cámara escogida
 
@@ -20,29 +20,31 @@ while(True):#Bucle infinito
         a, frame = capturar.read()#leo con la camara seleccionada:a es booleano, frame es un cuadro
         imagen=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#cambiar colores a escala de grises y guardar en variable imagen
 
-        ojos = clasificador_ojo.detectMultiScale(imagen, 1.3, 5)
+        #ojos = clasificador_ojo.detectMultiScale(imagen, 1.3, 5)
         rostro = clasificador_rostro.detectMultiScale(imagen, 1.3, 5)
 
 #Landmarks
-        clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(6,6))
-        clahe_image = clahe.apply(imagen)
-        detections = detector(clahe_image, 1)
+        
 
         registro = open('registro.txt','a')# a escribe desde el final, w bora y escribe
-
-        for k,d in enumerate(detections):
-                shape = predictor(clahe_image, d)
-                for i in range(1,68):
-                        cv2.circle(frame, (shape.part(i).x, shape.part(i).y), 1, (300,255,0), thickness=1)
-                        plt.scatter(shape.part(i).y,shape.part(i).x)
-        plt.show()
+        
+        if cv2.waitKey(1) & 0xFF == ord('m'):
+                clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(6,6))
+                clahe_image = clahe.apply(imagen)
+                detections = detector(clahe_image, 1)
+                for k,d in enumerate(detections):
+                        shape = predictor(clahe_image, d)
+                        for i in range(1,68):
+                                cv2.circle(frame, (shape.part(i).x, shape.part(i).y), 1, (300,255,0), thickness=1)
+                                plt.scatter(shape.part(i).y,shape.part(i).x,)
+                plt.show()
 
         for (x,y,w,h) in rostro:
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),1)
                 cortar= imagen[y:y+h, x:x+w]#Cortar coordenadas seleccionadas
                 cv2.imshow('Rostro', cortar)
                 
-                #print(time.strftime("ROSTRO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S")+'\n')
+                print(time.strftime("ROSTRO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S")+'\n')
                 registro.write(time.strftime("ROSTRO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S")+'\n')         
                 
                 cv2.putText(frame, Nombre, (x-5, y-5), cv2.FONT_HERSHEY_PLAIN,1,(300, 255, 0))#Insertar texto
@@ -54,7 +56,7 @@ while(True):#Bucle infinito
                 nfoto+=1
         if cv2.waitKey(1) & 0xFF == ord('s'):#Esperar que una tecla se presione y que esta sea la 's'
                 break#Saltar bucle
-        time.sleep(1)
+        #time.sleep(1)
 
 capturar.release()#Liberando recursos
 registro.close()
