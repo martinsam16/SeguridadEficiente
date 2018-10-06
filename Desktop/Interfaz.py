db72df55ab77f9582a2d12d16559a9998079cf47
master <{martin.saman@vallegrande.edu.pe}>
2018-10-06 03:51:35 -0500

    optimización

--- Desktop/Interfaz.py
+++ Desktop/Interfaz.py
@@ -1,12 +1,12 @@
 from Tkinter import *
 import cv2
 
-def Ventana(Nombre,nfoto,cortar):
-    ventana=Tk()
-    ventana=Frame(height=150,width=400)
-    ventana.pack(padx=20,pady=20)
-    boton=Button(ventana, text="Guardar",command=Guardar(Nombre,nfoto,cortar),font=("Verdana",5),height=6,width=6).pack()
+def ventana(Nombre, nfoto, cortar):
+    ventana = Tk()
+    ventana = Frame(height = 150, width = 400)
+    ventana.pack(padx = 20, pady = 20)
+    boton = Button(ventana, text = "Guardar", command = Guardar(Nombre, nfoto, cortar), font = ("Verdana", 5), height = 6, width = 6).pack()
     ventana.mainloop()
     
-def Guardar(Nombre,nfoto,cortar):
-    cv2.imwrite('img/'+Nombre+str(nfoto)+'.png',cortar)
+def Guardar(Nombre, nfoto, cortar):
+    cv2.imwrite('img/'+Nombre+str(nfoto)+'.png', cortar)
--- Desktop/Seguridad_Eficiente-0.1.py
+++ Desktop/Seguridad_Eficiente-0.1.py
@@ -1,30 +1,29 @@
-
 import cv2
 import numpy as np 
 import time
-#import Interfaz
+# import Interfaz
 import DibujarRostro
 
-ClasificadorRostro=cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_alt.xml')
-capturar=cv2.VideoCapture(0)
+ClasificadorRostro = cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_alt.xml')
+capturar = cv2.VideoCapture(0)
 
-Nombre='Nombre'#Será sustituido por un diccionario Nombres={"Maria":[x,y]}
-nfoto=0
+Nombre = 'Nombre'#Será sustituido por un diccionario Nombres = {"Maria":[x, y]}
+nfoto = 0
 while(True):
         a, frame = capturar.read()
-        imagen=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        imagen = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         rostro = ClasificadorRostro.detectMultiScale(imagen, 1.3, 5)
 
-        #registro = open('registro.txt','a')
+        #registro = open('registro.txt', 'a')
    
         if cv2.waitKey(1) & 0xFF == ord('m'):
-                DibujarRostro.Iniciar(imagen,frame)
+                DibujarRostro.Iniciar(imagen, frame)
 
-        for (x,y,w,h) in rostro:
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(300,255,0),1)
-                cv2.putText(frame, Nombre, (x-5, y-5), cv2.FONT_HERSHEY_PLAIN,1,(300, 255, 0))
+        for (x, y, w, h) in rostro:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (300, 255, 0), 1)
+                cv2.putText(frame, Nombre, (x-5, y-5), cv2.FONT_HERSHEY_PLAIN, 1, (300, 255, 0))
 
-                cortar= imagen[y:y+h, x:x+w]
+                cortar = imagen[y:y+h, x:x+w]
                 cv2.imshow('Rostro', cortar)
 
                 print(time.strftime("ROSTRO IDENTIFICADO    - Fecha: %d/%m/%y"+" Hora: %H:%M:%S")+'\n')
@@ -34,8 +33,8 @@ while(True):
         cv2.imshow('Seguridad Eficiente V-0.1', frame)
         """
         if cv2.waitKey(1) & 0xFF == ord('v'):
-                Interfaz.Ventana(Nombre,nfoto,cortar)
-                nfoto+=1
+                Interfaz.Ventana(Nombre, nfoto, cortar)
+                nfoto += 1
         """
         if cv2.waitKey(1) & 0xFF == ord('s'):
                 print "Saliendo .."
