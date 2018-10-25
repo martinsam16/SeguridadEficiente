21dfe0fa41607081cd268e098385671d81779ee1
Martín Alexis Samán Arata <42879133+MartinSamanArata2018@users.noreply.github.com>
2018-10-25 10:28:42 -0500

    Update Ventana.py

--- Desktop/Ventana.py
+++ Desktop/Ventana.py
@@ -1,7 +1,16 @@
-import Tkinter as tk, os,shutil as shu
+import Tkinter as tkint, os,shutil as shu
 
 def CrearCarpeta(nombre):
   os.mkdir("./img/"+nombre)
  
 def EliminarCarpeta(nombre):
   shu.rmtree("./img"+nombre)
+  
+def IngresarDatos():
+  
+  
+class IniciarVentana(imagen,nombre):
+    root=tkint.Tk()
+    btn=tkint.Button(root,text="Registrar",command=IngresarDatos())
+    btn.pack()
+    root.mainloop()
