84931880430c7efc3dc28bbd2b50f1da199772d4
Martín Alexis Samán Arata <42879133+MartinSamanArata2018@users.noreply.github.com>
2018-10-25 09:57:27 -0500

    Update BD_Rostros.sql

--- ConectarBD/BD_Rostros.sql
+++ ConectarBD/BD_Rostros.sql
@@ -6,7 +6,7 @@ CREATE TABLE USUARIO (
   NomUser VARCHAR(45) NOT NULL, /*nombre del usuario que es gravado o tomado foto*/
   ApellUser VARCHAR(45) NOT  NULL,/*Apellido del usuario que es gravado o tomado foto*/
   DNIUser VARCHAR(8) NOT NULL,/* DNI  del usuario que es gravado o tomado foto*/
-  FechNacUSer VARCHAR(45) NULL,/* Fecha de nacimiento del usuario del usuario que es gravado o tomado foto*/
+  FechNacUSer DATE NULL,/* Fecha de nacimiento del usuario del usuario que es gravado o tomado foto*/
   DirecUser VARCHAR(45) NULL,/*monbre del usuario que es gravado o tomado foto*/
   NroCellUser VARCHAR(9) NULL,/* Celular  del usuario que es gravado o tomado foto*/
   GenrUser boolean NOT NULL,/* Genero  del usuario que es gravado o tomado foto*/
